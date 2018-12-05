c5bded86eda0227ea29de46673ecb9b8b20efd57
BlueHedgehog <>
2018-12-05 18:36:49 +0800

    up

--- betdice/betdice/statusBar/BDStatusBarViewController.m
+++ betdice/betdice/statusBar/BDStatusBarViewController.m
@@ -194,54 +194,54 @@
     BDGameHistory *preRes = [BDGameHistory new];
     NSMutableString *resultStr = [NSMutableString string];
     
-// test s
-//    NSMutableArray *testlist = [NSMutableArray arrayWithCapacity:self.historyList.count];
-//test e
+    // test s
+    //    NSMutableArray *testlist = [NSMutableArray arrayWithCapacity:self.historyList.count];
+    //test e
     
     for (NSInteger i = 0 ; i < self.historyList.count; i ++) {
         BDGameHistory *res = self.historyList[i];
-//        test s
+        //        test s
         /*
-        // 用例1 庄 庄 闲 庄 闲            ok
-        // 用例2 庄 闲 庄 庄              ok
-        // 用例3 庄 平 庄 平 庄 闲         ok
-        // 用例4 庄 平 平 庄 平 庄 闲      ok
-        // 用例5 庄 平 平 平 庄 闲        ok
-        // 用例6 庄 平 平 庄 平 庄 闲     ok
-        // 用例7 庄 庄 平 庄 平 闲        ok
-        // 用例7 庄 庄 平 庄 庄 闲        ok
-        // 用例8 平 庄 平 平 庄 闲        ok
-        // 用例9 平 庄 庄 平 庄 闲        ok
-        // 用例9 平 平 平 庄 平 庄 闲     ok
-        // 用例10 平 平 庄 庄 平 庄 闲    ok
-        if (i == 0  )
-        {
-            res.result = @"tie";
-        }else if (i == 1){
-            res.result = @"tie";
-        }else if (i == 2){
-            res.result = @"banker";
-        }else if (i == 3){
-            res.result = @"banker";
-        }else if (i == 4){
-            res.result = @"tie"; //player  banker  tie
-        }else if (i == 5)
-        {
-            res.result = @"banker";
-        }else if (i ==6 )
-        {
-            res.result = @"player";
-        }
-        
-        [testlist addObject:res];
-        */
-//        test s
+// 用例1 庄 庄 闲 庄 闲            ok
+// 用例2 庄 闲 庄 庄              ok
+// 用例3 庄 平 庄 平 庄 闲         ok
+// 用例4 庄 平 平 庄 平 庄 闲      ok
+// 用例5 庄 平 平 平 庄 闲        ok
+// 用例6 庄 平 平 庄 平 庄 闲     ok
+// 用例7 庄 庄 平 庄 平 闲        ok
+// 用例7 庄 庄 平 庄 庄 闲        ok
+// 用例8 平 庄 平 平 庄 闲        ok
+// 用例9 平 庄 庄 平 庄 闲        ok
+// 用例9 平 平 平 庄 平 庄 闲     ok
+// 用例10 平 平 庄 庄 平 庄 闲    ok
+         if (i == 0  )
+         {
+         res.result = @"tie";
+         }else if (i == 1){
+         res.result = @"tie";
+         }else if (i == 2){
+         res.result = @"banker";
+         }else if (i == 3){
+         res.result = @"banker";
+         }else if (i == 4){
+         res.result = @"tie"; //player  banker  tie
+         }else if (i == 5)
+         {
+         res.result = @"banker";
+         }else if (i ==6 )
+         {
+         res.result = @"player";
+         }
+         
+         [testlist addObject:res];
+         */
+        //        test s
         [resultStr appendString:[self changeString:res.result]];
     }
-//    test s
-//    [self.historyList removeAllObjects];
-//    [self.historyList addObjectsFromArray:testlist];
-//    test e
+    //    test s
+    //    [self.historyList removeAllObjects];
+    //    [self.historyList addObjectsFromArray:testlist];
+    //    test e
     
     self.recWin.stringValue = [NSString stringWithFormat:@"最近Win：%@",[self changeString:self.historyList.firstObject.result]];
     self.resultText.stringValue = resultStr;
@@ -249,16 +249,14 @@
         BDGameHistory *res = self.historyList[j];
         // 上一条记录
         if(j == 0 ){preRes = res;   [winlist addObject:res.result];; continue;}
-       
+        
         NSString *sameRes = winlist.firstObject;
         // 条件1 前后，中间是平，都累加 或者2数相等
         if ([preRes.result isEqualToString:res.result] || (self.tieCheck.state == YES && [res.result isEqualToString:@"tie"])||
             (self.tieCheck.state == YES && [preRes.result isEqualToString:@"tie"]))
         {
-            
             BOOL isSame = true;
             for (NSInteger i = 0 ; i <  winlist.count ; i ++) {
-                
                 NSString *res = winlist[i];
                 // 平局忽略，首个平局忽略
                 if ([res isEqualToString:@"tie"]  || (i == 0 && [sameRes isEqualToString:winlist[i] ]) ) {
@@ -271,35 +269,12 @@
             {
                 [winlist addObject:res.result];
             }else {
-//                NSString *body = [NSString stringWithFormat:@"当前 %@ 已经连赢%ld 次",[self changeString:sameRes],winNum];
-//                NSLog(@"shang tiaojie %@",body );
-//                if (winNum >= self.tipsNumBtn.selectedItem.title.integerValue) {
-//                    if (self.timestamp == 0 ||   [NSDate date].timestamp.integerValue- self.timestamp > self.tipsTimeBtn.selectedItem.title.integerValue) {
-//                        self.timestamp = [NSDate date].timestamp.integerValue ;
-//                        [self pushNotification:@"警报！！！！" body:body];
-//                    }
-//                }
-//                self.conWin.stringValue = body;
-//                [winlist removeAllObjects];
-//                winNum = 1;
                 [self pushNotiWithWinner:[self changeString:sameRes] winNum:winNum winlist:winlist];
                 break;
             }
             winNum ++;
         }
         else {
-            // 连赢，只有庄或者闲 ，次数加上平
-//            NSString *body = [NSString stringWithFormat:@"当前 %@ 已经连赢%ld 次",[self changeString:preRes.result],winNum];
-//            NSLog(@"xia tiaojie %@",body );
-//            if (winNum >= self.tipsNumBtn.selectedItem.title.integerValue) {
-//                if (self.timestamp == 0 ||   [NSDate date].timestamp.integerValue- self.timestamp > self.tipsTimeBtn.selectedItem.title.integerValue) {
-//                    self.timestamp = [NSDate date].timestamp.integerValue ;
-//                    [self pushNotification:@"警报！！！！" body:body];
-//                }
-//            }
-//            self.conWin.stringValue = body;
-//            [winlist removeAllObjects];
-//            winNum = 1; break;
             [self pushNotiWithWinner:[self changeString:preRes.result] winNum:winNum winlist:winlist];
             break;
         }
